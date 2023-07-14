6c179379b1021efd667f44c8d09e938b97e76789
yennyparra <yennyparra79@gmail.com>
2023-07-14 08:57:27 -0400

    primer commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+   resources :articles
+   devise_for :users
+   post 'new_user_raction',  to: "reactions#new_user_reaction", as:"new_user_reaction"
+   resources :comments, only: [:create]
+  
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "articles#index"
+end
